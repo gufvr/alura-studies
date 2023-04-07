694ae2d183ccfaae29545490fb5d4b4931713a36
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-04-07 13:59:29 -0300

    Renderizando arrays para criação de listas e economia de códigos

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,29 +1,33 @@
-import React from 'react';
+import React from "react";
 
 function List() {
+  const tasks = [
+    {
+      task: "React",
+      time: "02:00:00",
+    },
+    {
+      task: "Javascript",
+      time: "01:00:00",
+    },
+    {
+      task: "Typescript",
+      time: "03:00:00",
+    },
+  ];
   return (
     <aside>
-     <h2>Estudos do dia</h2> 
-     <ul>
-      <li>
-        <h3>
-          React
-        </h3>
-        <span>
-          02:00:00
-        </span>
-      </li>
-      <li>
-        <h3>
-          JavaScript
-        </h3>
-        <span>
-          01:00:00
-        </span>
-      </li>
-     </ul>
+      <h2>Estudos do dia</h2>
+      <ul>
+        {tasks.map((item, index) => (
+          <li key={index}>
+            <h3>{item.task}</h3>
+            <span>{item.time}</span>
+          </li>
+        ))}
+      </ul>
     </aside>
-  )
+  );
 }
 
 export default List;
