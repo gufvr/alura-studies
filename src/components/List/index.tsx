4b27523c4c7a3b6a6717314652171669745b9c0c
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-04-07 18:14:25 -0300

    Introduzindo estados no projeto e começando a dar vida ao App

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import style from './List.module.scss';
 import Item from "./item";
 
 function List() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       task: "React",
       time: "02:00:00",
@@ -16,10 +16,12 @@ function List() {
       task: "Typescript",
       time: "03:00:00",
     },
-  ];
+  ]);
   return (
     <aside className={style.taskList}>
-      <h2>Estudos do dia</h2>
+      <h2 onClick={() => {
+        setTasks([...tasks, { task: "Estudar estado", time: "05:00:00" }]);
+      }}>Estudos do dia</h2>
       <ul>
         {tasks.map((item, index) => (
           <Item 
