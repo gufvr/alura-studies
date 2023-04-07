e7bbc42ab2e9641a4287842bc958bc780db14d40
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-04-07 12:52:39 -0300

    Entendendo sobre DRY e criando o componente Button.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Button />
     </div>
   );
 }
