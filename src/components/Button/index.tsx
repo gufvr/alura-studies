ba866b542033ccc1078022531f41895e6cb4b8aa
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-04-07 15:37:43 -0300

    Instalando o Sass e estilizando componentes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,10 @@
 import React from "react";
+import './style.scss';
 
 class Button extends React.Component {
   render() {
-    const isActive = true;
-    const styles = {
-      backgroundColor: isActive ? "green" : "red"
-    }
     return (
-      <button style={styles}>
+      <button className="button">
         Botão
       </button>
     )
