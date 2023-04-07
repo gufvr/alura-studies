d54c65c662a1ac563be3a9cf7e96519f77bef7bf
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-04-07 18:51:50 -0300

    Controlando o formulário com uso de states

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,9 +3,19 @@ import Button from "../Button";
 import style from'./Form.module.scss';
 
 class Form extends React.Component {
+  state = {
+    task: "",
+    time: "00:00:00"
+  }
+
+  addTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log('state: ', this.state);
+  }
+
   render() {
     return (
-      <form className={style.newTask}>
+      <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">
             Adicione um novo estudo
@@ -13,6 +23,8 @@ class Form extends React.Component {
           <input 
             type="text"
             name="tarefa"
+            value={this.state.task}
+            onChange={event => this.setState({ ...this.state, task: event.target.value})}
             id="tarefa"
             placeholder="O que você quer estudar?"
             required
@@ -26,6 +38,8 @@ class Form extends React.Component {
             type="time"
             step="1"
             name="tempo"
+            value={this.state.time}
+            onChange={event => this.setState({ ...this.state, time: event.target.value })}
             id="tempo"
             min="00:00:00"
             max="01:30:00"
