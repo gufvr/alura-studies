238e68b8c1ef71adef3153c12e4b90b34ecd58c6
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-04-07 13:07:49 -0300

    Criando o componente Form para formulário

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Button from './components/Button';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <Button />
+      <Form />
     </div>
   );
 }
