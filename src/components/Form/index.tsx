e929467dcb0eea1f088f0525986a1da9f05228ac
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-04-07 23:06:25 -0300

    Criando IDs aleatórios com uuidv4 (Identificador Único Universal) para atualizar tarefas e suas criações

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,18 +2,33 @@ import React from "react";
 import Button from "../Button";
 import style from "./Form.module.scss";
 import ITask from "../../types/task";
+import { v4 as uuidv4 } from "uuid";
 
 class Form extends React.Component<{
   setTasks: React.Dispatch<React.SetStateAction<ITask[]>>;
 }> {
   state = {
     task: "",
-    time: "00:00:00",
+    time: "00:00",
   };
 
   addTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    this.props.setTasks(oldTasks => [...oldTasks, { ...this.state }])
+    this.props.setTasks(oldTasks => 
+      [
+        ...oldTasks, 
+        { 
+          ...this.state,
+          selected: false,
+          completed: false,
+          id: uuidv4()
+        }
+      ]
+    )
+    this.setState({
+      task: "",
+      time: "00:00"
+    })
   }
 
   render() {
--- src/types/task.ts
+++ src/types/task.ts
@@ -1,4 +1,7 @@
 export default interface ITask {
-  task: string;
-  time: string;
+  task: string,
+  time: string,
+  selected: boolean,
+  completed: boolean,
+  id: string
 }
